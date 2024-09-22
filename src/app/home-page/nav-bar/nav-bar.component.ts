9dfd95fbbb4d1ef2a3683c79f754ca59836e1c37
menady0 <menady20@gmail.com>
2024-09-23 01:06:10 +0300

    added the 'active' for the nav and making it working properly

--- src/app/home-page/nav-bar/nav-bar.component.ts
+++ src/app/home-page/nav-bar/nav-bar.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+  constructor(){
+    document.addEventListener('DOMContentLoaded', () => {
+      let linksContainer = document.querySelector('ul');
+      if(linksContainer){
+      let links = linksContainer.querySelectorAll('a');
+      linksContainer.addEventListener('click', function(event) {
+        if(event.target != null){
+          const target = event.target as HTMLElement;
+          if (target.tagName === 'A') {    
+            links.forEach(link => link.classList.remove('active'));
+            target.classList.add('active');
+          }
+        }
+      });
+    }
+    else
+    console.log("ul is null");
+    });
+  }
 }
